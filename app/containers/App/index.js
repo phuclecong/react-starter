743839a3fc6f52ea8a872c9953515139e4d4d9ad
Phuc Le (Lazada Group) <phuc.le@laz-vn-l-m-1908.lazada.intlaz>
2019-07-01 16:11:53 +0700

    Add sass-loader

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,7 +13,8 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-import GlobalStyle from '../../global-styles';
+// import GlobalStyle from '../../global-styles';
+import './index.scss';
 
 export default function App() {
   return (
@@ -22,7 +23,6 @@ export default function App() {
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
-      <GlobalStyle />
     </div>
   );
 }
